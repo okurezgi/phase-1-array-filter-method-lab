9ecc033a7c1fe3fe0d7b8af1f08b4bbcad5a1df2
“Ezgi <okurezgi@windowslive.com>
2022-07-10 23:35:28 +0300

    assingment solved

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+const findMatching = (drivers, searchingString)=>{
+    return drivers.filter( driver => 
+        driver.toUpperCase() === searchingString.toUpperCase())
+   }
+   
+   const fuzzyMatch =  (drivers, searchingString)=>{
+       return drivers.filter(driver =>
+          driver.startsWith(searchingString))
+      }
+   
+   const matchName =  (drivers, searchingString)=>{
+       return drivers.filter(driver =>
+          driver.name === searchingString)
+      }
+   
+   
